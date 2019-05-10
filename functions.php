4e80f64137c3620daeebac3f779e86b576a9c988
Keith <keith.maiabch@hazelwood.ca>
2019-05-10 10:00:48 -0700

    Add lots of stuff, 25% done

--- functions.php
+++ functions.php
@@ -1,20 +1,20 @@
 <?php
 
 function university_files(){
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
-    wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+  wp_enqueue_style('custom_google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 function university_features(){
-    // This gives us a menus option is the appearance section on admin page
-    // These correspond to locations where <ul> lists can be injected automagically through this admin menu page
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocation1', 'Footer Location One');
-    register_nav_menu('footerLocation2', 'Footer Location Two');
-    //This adds support for auto page titles
-    add_theme_support('title-tag');
+  // This gives us a menus option is the appearance section on admin page
+  // These correspond to locations where <ul> lists can be injected automagically through this admin menu page
+  register_nav_menu('headerMenuLocation', 'Header Menu Location');
+  register_nav_menu('footerLocation1', 'Footer Location One');
+  register_nav_menu('footerLocation2', 'Footer Location Two');
+  //This adds support for auto page titles
+  add_theme_support('title-tag');
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
